658f744205b5a02748c4f04ce4a67581ef137ea5
Viktar Akulenka <viktor.akulenko@bonacasa.ch>
2020-06-29 17:45:33 +0300

    user-phone: seeder & factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,18 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+//         $this->call(UserSeeder::class);
 
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Hash::make('password'),
-        ]);
+//        DB::table('users')->insert([
+//            'name' => Str::random(10),
+//            'email' => Str::random(10).'@gmail.com',
+//            'password' => Hash::make('password'),
+//        ]);
+//
+//        DB::table('users')->insert([
+//            'name' => 'name' . Str::random(4),
+//            'email' => 'email' . Str::random(4).'@gmail.com',
+//            'password' => Hash::make('password'),
+//        ]);
 
-        DB::table('users')->insert([
-            'name' => 'name' . Str::random(4),
-            'email' => 'email' . Str::random(4).'@gmail.com',
-            'password' => Hash::make('password'),
+        $this->call([
+            UserSeeder::class
         ]);
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,5 +12,13 @@ class UserSeeder extends Seeder
     public function run()
     {
         //
+
+//        factory(App\User::class, 5)->create();
+
+        factory(App\User::class, 5)->create()->each(function ($user) {
+            $user->phone()->save(factory(App\Phone::class)->make());
+        });
+
+
     }
 }
