755fefdbb9a319b2de13c6040931b987194d8758
Viktar Akulenka <repetitor202@gmail.com>
2020-07-18 10:55:48 +0300

    tests - refactoring2

--- tests/Feature/SeparatedTestAuth.php
+++ tests/Feature/SeparatedTestAuth.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Tests\SeparatedLaunch;
+namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
-class AuthTest extends TestCase
+class SeparatedTestAuth extends TestCase
 {
 //    /**
 //     * If true, setup has run at least once.
