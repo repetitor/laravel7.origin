4f630d96877e57ded18ccdbfe8195b70ab8a5562
Viktar Akulenka <repetitor202@gmail.com>
2020-07-19 15:17:22 +0300

    Auth/LoginTest

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    protected static $isUserRegistered = false;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (!static::$isUserRegistered) {
+            $this->postJson(AuthHelper::URI_REGISTER, AuthHelper::REQUEST_REGISTER);
+            static::$isUserRegistered = true;
+        }
+    }
+
+//    public function tearDown(): void
+//    {
+//        parent::refresh();
+//    }
+
+    public function test_success()
+    {
+        $request = AuthHelper::REQUEST_LOGIN;
+        $response = $this->postJson(AuthHelper::URI_LOGIN, $request);
+
+        $response->assertStatus(200);
+        $response->assertSeeText('access_token');
+        $this->assertTrue(isset($response['user']));
+        $this->assertTrue(isset($response['access_token']));
+    }
+
+    public function test_success2()
+    {
+        $request = AuthHelper::REQUEST_LOGIN;
+        $response = $this->postJson(AuthHelper::URI_LOGIN, $request);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_error_wrong_password()
+    {
+        $request = AuthHelper::REQUEST_LOGIN;
+        $request['password'] = 'wrongPassword';
+        $response = $this->postJson(AuthHelper::URI_LOGIN, $request);
+
+        $response->assertStatus(200);
+        $this->assertTrue($response['message'] == 'Invalid login credentials.');
+    }
+
+    public function test_error_empty_password()
+    {
+        $request = AuthHelper::REQUEST_LOGIN;
+        $request['password'] = '';
+        $response = $this->postJson(AuthHelper::URI_LOGIN, $request);
+
+        $response->assertStatus(422);
+        $this->assertTrue($response['message'] == 'The given data was invalid.');
+        $this->assertTrue($response['errors']['password'][0] == 'The password field is required.');
+    }
+
+    public function test_error_absent_password()
+    {
+        $request = AuthHelper::REQUEST_LOGIN;
+        unset($request['password']);
+        $response = $this->postJson(AuthHelper::URI_LOGIN, $request);
+
+        $response->assertStatus(422);
+        $this->assertTrue($response['message'] == 'The given data was invalid.');
+        $this->assertTrue($response['errors']['password'][0] == 'The password field is required.');
+    }
+}
